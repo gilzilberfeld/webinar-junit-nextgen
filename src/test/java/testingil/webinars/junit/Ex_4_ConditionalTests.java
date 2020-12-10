220cfb8e147563767befd861fcb69372aa497225
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-12-10 12:41:58 +0200

    conditinoal and grouped tests

--- src/test/java/testingil/webinars/junit/Ex_4_ConditionalTests.java
+++ src/test/java/testingil/webinars/junit/Ex_4_ConditionalTests.java
@@ -1,16 +1,36 @@
 package testingil.webinars.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 class Ex_4_ConditionalTests {
+	private Calculator calc;
 
-	//4.  Conditional tests (@Enabled...)
+	@BeforeEach
+	public void setup() {
+		calc = new Calculator();
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@EnabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_13)
+	@DisplayName("So glad your family ain't around to see this")
+	void an_embarassing_test() {
+		assertThat(calc.add(2, 3), is(5));
+	}
+
+	@Test
+	@EnabledOnOs(OS.LINUX)
+	@DisplayName("Think of the children...")
+	void a_humilating_test() {
+		assertThat(calc.add(4, 6), is(10));
 	}
 
 }
--- src/test/java/testingil/webinars/junit/Ex_5_GroupedTests.java
+++ src/test/java/testingil/webinars/junit/Ex_5_GroupedTests.java
@@ -1,16 +1,37 @@
 package testingil.webinars.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Ex_5_GroupedTests {
 
-	//5.  Grouped tests (assertAll)
+	private Calculator calc;
+
+	@BeforeEach
+	public void setup() {
+		calc = new Calculator();
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("You're showing those tests to ")
+	void an_embarassing_test() {
+		Assertions.assertAll("real people?", 
+				() -> assertThat(calc.add(2, 3), is(5)),
+				() -> assertThat(calc.add(1, 4), is(5)));
+	}
+
+	@Test
+	@DisplayName("What would your mother say?")
+	void a_humilating_test() {
+		Assertions.assertAll("This would make her cry", 
+				() -> assertThat(calc.add(2, 3), is(not(6))),
+				() -> Assertions.assertDoesNotThrow(() -> calc.add(8, 9)));
 	}
 
 }
