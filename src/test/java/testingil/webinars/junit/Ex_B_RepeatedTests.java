763aedbec0470befc9eea080dfa8e0356a7c734d
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-12-11 17:52:06 +0200

    Added parameterized and repeated tests

--- src/test/java/testingil/webinars/junit/Ex_B_RepeatedTests.java
+++ src/test/java/testingil/webinars/junit/Ex_B_RepeatedTests.java
@@ -1,16 +1,40 @@
 package testingil.webinars.junit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class Ex_B_RepeatedTests {
-
-	//B.  Repeated tests (@RepeatedTest)
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Calculator calc;
+	
+	@BeforeEach
+	public void setup() {
+		calc = new Calculator();
 	}
-
+	
+	@ParameterizedTest
+	@CsvSource({"1, 2, 3", 
+				"2, 4, 6",
+				"3, 6, 9"}) 
+	void multi_emberassing_test(int a, int b, int expected) {
+		String error = "I'm sorry! ";
+		assertThat(error, calc.add(a, b), is(expected));
+	}
+	
+	
+	@RepeatedTest(3)
+	void repeateadly_humiliating_test(RepetitionInfo rep) {
+		int current = rep.getCurrentRepetition();
+		String error = "Sorry for the " + current +" time"; 
+		assertThat(error, calc.add(2,current ), lessThan (10)); 
+	 
+	}
 }
