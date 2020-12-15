64f66473dbe0ab7e5152a5e85b21a2d24c8833b7
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-12-15 15:44:38 +0200

    Update Ex_0_JUnit5Test.java
    Used Assertions

--- src/test/java/testingil/webinars/junit/Ex_0_JUnit5Test.java
+++ src/test/java/testingil/webinars/junit/Ex_0_JUnit5Test.java
@@ -1,7 +1,6 @@
 package testingil.webinars.junit;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class Ex_0_JUnit5Test {
@@ -9,7 +8,7 @@ class Ex_0_JUnit5Test {
 	@Test
 	void an_embarassing_test() {
 		Calculator calc = new Calculator();
-		assertEquals(5, calc.add(2, 3));
+		Assertions.assertEquals(5, calc.add(2, 3));
 	}
 
 }
